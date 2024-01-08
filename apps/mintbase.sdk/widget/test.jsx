d5d76a3699c0387990f51eb46f634841a6a0db18
M-RB3 <rabea9220@gmail.com>
2024-01-08 09:21:17 +0200

    update test

--- apps/mintbase.sdk/widget/test.jsx
+++ apps/mintbase.sdk/widget/test.jsx
@@ -1,11 +1,43 @@
-const [media, setMedia] = useState("");
-const [title, setTitle] = useState("");
-const [desc, setDesc] = useState("");
+const [state, setState] = useState({
+  media: "",
+  title: "",
+  desc: "",
+  name: "",
+  symbol: "",
+  tokenId: "3",
+  contractAddress: "nakma2321.mintspace2.testnet",
+  address: "",
+  price: "",
+  recevierId: "",
+  tokenData: [],
+  storeNFTs: [],
+  ownedNFTs: [],
+});
+
+const {
+  media,
+  title,
+  desc,
+  name,
+  symbol,
+  tokenId,
+  contractAddress,
+  address,
+  price,
+  tokenData,
+  storeNFTs,
+  ownedNFTs,
+  recevierId,
+} = state;
+
+const updateState = (args) => {
+  setState({ ...state, ...args });
+};
 const [sdk, setSDK] = useState(false);
 
 const filesOnChange = (files) => {
   if (files) {
-    setMedia(files[0]);
+    updateState({ media: files[0] });
   }
 };
 
@@ -14,19 +46,65 @@ const handleSubmit = () => {
     title: title,
     description: desc,
   };
-  sdk.mint(tokenMetadata, media);
+  const mint = sdk.mint(tokenMetadata, media, contractAddress);
+  console.log("mint", mint);
 };
 
+const handleDeploy = () => {
+  const deploy = sdk.deployStore(name, symbol);
+};
+const handleFetch = (type) => {
+  let res;
+  switch (type) {
+    case "get-token":
+      res = sdk.getTokenById(contractAddress, tokenId);
+      res.then((res) => {
+        updateState({ tokenData: res.body.data.mb_views_nft_tokens });
+      });
+      break;
+    case "get-store-nft":
+      res = sdk.getStoreNfts(contractAddress);
+      res.then((res) => {
+        updateState({
+          storeNFTs: res.body.data.mb_views_nft_metadata_unburned,
+        });
+      });
+      break;
+    case "get-owned-nft":
+      res = sdk.getOwnedNFTs("leo_phoenix.near");
+      res.then((res) => {
+        updateState({
+          ownedNFTs: res.body.data.mb_views_nft_tokens,
+        });
+      });
+      break;
+    case "list-nft":
+      sdk.nftApprove(tokenId, contractAddress, price);
+      break;
+    case "nft-burn":
+      sdk.nftBurn([tokenId], contractAddress);
+      break;
+    case "nft-transfer":
+      console.log(recevierId);
+      sdk.nftTransfer(tokenId, recevierId, contractAddress);
+      break;
+    default:
+      break;
+  }
+};
 return (
   <div>
     <Widget
       src="test.near/widget/SDK"
       props={{
+        mainnet: false,
+        contractName: "nakma2321.mintspace2.testnet",
+        loaded: sdk,
         onLoad: (sdk) => setSDK(sdk),
         onRefresh: (sdk) => setSDK(sdk),
-        loaded: sdk,
       }}
     />
+    <h1 className="mt-4"> MINT</h1>
     <div
       style={{
         display: "flex",
@@ -41,9 +119,8 @@ return (
       </Label.Root>
       <input
         className="Input"
-        onChange={(e) => setTitle(e.target.value)}
+        onChange={(e) => updateState({ title: e.target.value })}
         type="text"
-        id="firstName"
         defaultValue=""
       />
     </div>
@@ -62,22 +139,30 @@ return (
       <input
         className="Input"
         type="text"
-        id="firstName"
-        onChange={(e) => setDesc(e.target.value)}
+        onChange={(e) => updateState({ desc: e.target.value })}
         defaultValue=""
       />
     </div>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        Contract Address
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ contractAddress: e.target.value })}
+        type="text"
+        value={contractAddress}
+      />
+    </div>
     <div className="d-inline-block">
-      {media ? (
-        <img
-          class="rounded w-100 h-100"
-          style={{ objectFit: "cover" }}
-          src={`https://ipfs.near.social/ipfs/${state.img.cid}`}
-          alt="upload preview"
-        />
-      ) : (
-        ""
-      )}
       <Files
         multiple={false}
         accepts={["image/*"]}
@@ -86,9 +171,312 @@ return (
         className="btn btn-outline-primary"
         onChange={filesOnChange}
       >
-        {media?.uploading ? <> Uploading </> : "Upload an Image"}
+        Upload an Image
       </Files>
     </div>
-    <input type="submit" onClick={() => handleSubmit()} />
+    <input type="submit" onClick={() => handleSubmit()} value="mint" />
+    <h1 className="mt-4">DEPLOY</h1>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        name
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ name: e.target.value })}
+        type="text"
+        defaultValue=""
+      />
+    </div>{" "}
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        symbol
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ symbol: e.target.value })}
+        type="text"
+        defaultValue=""
+      />
+    </div>
+    <h1 className="mt-4">GET TOKEN BY ID</h1>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        Token ID
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ tokenId: e.target.value })}
+        type="text"
+        value={tokenId}
+      />
+    </div>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        Contract Address
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ contractAddress: e.target.value })}
+        type="text"
+        value={contractAddress}
+      />
+    </div>
+    <input type="submit" onClick={() => handleFetch("get-token")} value="get" />
+    <div>{JSON.stringify(tokenData[0])}</div>
+    <h1 className="mt-4"> GET STORE NFTs</h1>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        Contract Address
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ contractAddress: e.target.value })}
+        type="text"
+        value={contractAddress}
+      />
+    </div>
+    <input
+      type="submit"
+      onClick={() => handleFetch("get-store-nft")}
+      value="get"
+    />
+    <div>{JSON.stringify(storeNFTs)}</div>
+    <h1 className="mt-4"> GET OWNED NFTs</h1>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        Address
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ address: e.target.value })}
+        type="text"
+        placeholder="provide address or signed in address"
+        value={address}
+      />
+    </div>
+    <input
+      type="submit"
+      onClick={() => handleFetch("get-owned-nft")}
+      value="get"
+    />
+    <div>{JSON.stringify(ownedNFTs)}</div>
+    <h1 className="mt-4">List NFT</h1>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        Token Id
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ tokenId: e.target.value })}
+        type="text"
+        placeholder=""
+        value={tokenId}
+      />
+    </div>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        Token Contract Id
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ contractAddress: e.target.value })}
+        type="text"
+        placeholder=""
+        value={contractAddress}
+      />
+    </div>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        price in near
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ price: e.target.value })}
+        type="text"
+        placeholder=""
+        value={price}
+      />
+    </div>
+    <input type="submit" onClick={() => handleFetch("list-nft")} value="list" />
+    <h1 className="mt-4">NFT BURN</h1>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        Token Id
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ tokenId: e.target.value })}
+        type="text"
+        placeholder=""
+        value={tokenId}
+      />
+    </div>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        Conrtact Address
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ contractAddress: e.target.value })}
+        type="text"
+        placeholder=""
+        value={contractAddress}
+      />
+    </div>{" "}
+    <input type="submit" onClick={() => handleFetch("nft-burn")} value="list" />
+    <h1 className="mt-4">NFT TRANSFER</h1>
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        TOKEN ID
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ tokenId: e.target.value })}
+        type="text"
+        placeholder=""
+        value={tokenId}
+      />
+    </div>{" "}
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        accountId
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ recevierId: e.target.value })}
+        type="text"
+        placeholder=""
+        value={recevierId}
+      />
+    </div>{" "}
+    <div
+      style={{
+        display: "flex",
+        padding: "0 20px",
+        flexWrap: "wrap",
+        gap: 15,
+        alignItems: "center",
+      }}
+    >
+      <Label.Root className="LabelRoot" htmlFor="firstName">
+        Conrtact Address
+      </Label.Root>
+      <input
+        className="Input"
+        onChange={(e) => updateState({ contractAddress: e.target.value })}
+        type="text"
+        placeholder=""
+        value={contractAddress}
+      />
+    </div>{" "}
+    <input
+      type="submit"
+      onClick={() => handleFetch("nft-transfer")}
+      value="list"
+    />
   </div>
 );
