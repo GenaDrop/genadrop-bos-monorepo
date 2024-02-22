8d7060a0c6676738b024fce851fb5ca0b5a15e92
ebube <ebubeagwaze@gmail.com>
2024-02-22 12:58:37 +0100

    update: working on the hero page

--- build/BosGenaDrop/src/Mintbase/App/Home/Index.jsx
+++ build/BosGenaDrop/src/Mintbase/App/Home/Index.jsx
@@ -1,4 +1,7 @@
 const { mode } = props;
+const { getInputLabelFontType } = VM.require(
+  "bos.genadrop.near/widget/Mintbase.components"
+);
 
 const Home = styled.div`
   display: flex; /* flex */
@@ -40,7 +43,56 @@ const Home = styled.div`
       z-index: 10; /* z-10 */
     }
   }
+  .card {
+    height: 100%; /* h-full */
+    cursor: pointer; /* cursor-pointer */
+    padding: 16px; /* p-24 */
+    color: #d1d5db; /* text-purple-100 */
+    background-color: rgb(63 56 56 / 7%); /* bg-opacity-50 */
+    z-index: 30; /* z-30 */
+    transition: 0.5s ease-in-out;
+    border-radius: 8px; /* rounded */
+    border: 1px solid ${mode === "dark" ? "#8b5cf6" : ""}; /* dark:border-purple-100 */
+    color: #8b5cf6;
+
+    &:hover {
+      background-color: #1e3a8a; /* hover:bg-mb-blackblue */
+      color: white; /* dark:text-purple-100 */
+
+      color: white !important;
+    }
+    .innerCard {
+      text-align: center; /* text-center */
+      word-break: break-word; /* break-words */
+      vertical-align: middle; /* align-middle */
+      backdrop-filter: blur(50px);
+
+      text-align: center; /* align-text-center */
+      .cardText {
+        ${getInputLabelFontType("big")}
+        font-weight: bold;
+        padding-right: 24px; /* pr-24 */
+        padding-left: 24px; /* pl-24 */
+      }
+    }
+  }
+  .cards {
+    display: grid;
+    grid-template-columns: repeat(2, 200px);
+    grid-gap: 10px;
+    margin-top: 20px;
+    margin-bottom: 70px;
+  }
 `;
+
+const cardItems = [
+  { name: "Creator Suite", link: "" },
+  { name: "Developers", link: "" },
+  { name: "Mint a Selfie", link: "" },
+  { name: "Market", link: "" },
+  { name: "Wallet", link: "" },
+  { name: "AI", link: "" },
+];
 
 return (
   <Home>
@@ -49,6 +101,17 @@ return (
       <div className="subText">
         An ownership and scarcity hub for creators and developers pioneering
         utility NFTs in AI, payments, rewards, tickets, art, memberships...
+      </div>
+      <div className="cards">
+        {cardItems.map((data) => (
+          <a>
+            <div className="card">
+              <div className="innerCard">
+                <div className="cardText">{data.name}</div>
+              </div>
+            </div>
+          </a>
+        ))}
       </div>
     </div>
   </Home>
