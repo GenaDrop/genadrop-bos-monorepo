6208755ee4edcdbfd0b24564b7b9711d0cbabcd3
Jiku Godwill <jikugodwill@yahoo.com>
2024-03-19 14:41:54 +0100

    Refactor CreateStoreCard component styles

--- apps/BosGenaDrop/widget/Mintbase/App/Store/CreateStoreCard.jsx
+++ apps/BosGenaDrop/widget/Mintbase/App/Store/CreateStoreCard.jsx
@@ -7,30 +7,9 @@ justify-content: center;
 align-content: center;
 gap: 2em;
 margin: 10px auto;
-border-radius: .7em;
 height: 293px;
 max-width: 600px;
 width: 100%;
-button {
-    border: 1px solid black;
-    border-radius: 0;
-    color: white;
-    background: black;
-    text-align: center
-    display: flex;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-  button:disabled {
-    background: grey;
-    border: grey;
-    cursor: not-allowed;
-  }
-  button:hover {
-    background: white;
-    color: black;
-    border-color: black;
-  }
 `;
 
 const CreateStoreCard = ({ isDarkModeOn, createStoreHandler }) => (
