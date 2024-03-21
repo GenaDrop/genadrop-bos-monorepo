9ee85996ee235040ddc27245c37581d30df268e7
Jiku Godwill <jikugodwill@yahoo.com>
2024-03-21 16:53:23 +0100

    Added user details

--- apps/BosGenaDrop/widget/Mintbase/App/Profile/Index.jsx
+++ apps/BosGenaDrop/widget/Mintbase/App/Profile/Index.jsx
@@ -31,31 +31,17 @@ const tabProps = { tabLabels: hiddenTabs };
 console.log("tabProps", tabProps);
 
 const [selectedTab, setSelectedTab] = useState(props.tab ?? "owned");
-const [switchOn, setSwitchOn] = useState(false);
 const [open, setOpen] = useState(false);
 const [sdk, setSDK] = useState(false);
 const [storeName, setStoreName] = useState("");
 const [storeSymbol, setStoreSymbol] = useState("");
 const [showOwnedFilters, setShowOwnedFilters] = useState(true);
-const mode = switchOn ? "light" : "dark";
-const btnType = !switchOn ? "secondary" : "primary";
-const isDarkModeOn = mode === "dark";
-
-// const [trialText, setTrialText] = useState("");
-
-// const handleTrialTextChange = ({ target: { value: text } }) => {
-//   console.log("handleTrialTextChange", text);
-//   setTrialText(text);
-//   setCount(text.length);
-// };
+const [profile, setProfile] = useState(null);
+const isDarkModeOn = props.isDarkModeOn ?? false;
 
 const handleTabClick = (index) => {
   setSelectedTab(index);
   // console.log("selectedTab from Mine: ", selectedTab);
-};
-
-const switchChangeHandler = () => {
-  setSwitchOn((prev) => !prev);
 };
 
 const onStoreNameChange = useCallback((e) => {
@@ -82,7 +68,7 @@ const Card = styled.div`
     color: white;
   }
   .content_main {
-    padding: 20px;
+    padding: 24px 48px;
   }
 `;
 
@@ -107,21 +93,96 @@ const CreateStore = styled.div`
   }
 `;
 
-const Toggle = styled.div`
-  position: fixed;
-  bottom: 1rem;
-  right: 1rem;
-  padding: 0.5rem;
-  background-color: black;
-  border-radius: 9999px;
-  cursor: pointer;
-  z-index: 100000;
-  &:hover {
-    background-color: #1f2937;
+const ImageSection = styled.div`
+  height: 254px;
+  background: #eceef0;
+  position: relative;
+  z-index: 1;
+  img {
+    width: 126px;
+    height: 126px;
+    border-radius: 50%;
+    position: absolute;
+    bottom: -24px;
+    border: 3px solid #1e2030;
+    left: 48px;
+    background: #aa4747;
+    margin: 0 auto;
   }
-  width: 2rem;
-  height: 2rem;
-  color: black;
+
+  @media (max-width: 1024px) {
+    margin: calc(var(--body-top-padding) * -1) -12px 0;
+    border-radius: 0;
+  }
+
+  @media (max-width: 1024px) {
+    height: 100px;
+  }
+`;
+
+const TopContent = styled.div`
+  margin-top: 40px;
+  h1 {
+    font-size: 20px;
+  }
+  .contents {
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+    .content {
+      margin: 0;
+      display: flex;
+      gap: 20px;
+      p {
+        margin: 0;
+      }
+    }
+  }
+`;
+
+const Details = styled.div`
+  display: flex;
+  gap: 30px;
+  margin-top: 20px;
+  flex-wrap: wrap;
+  .detail {
+    display: flex;
+    flex-direction: column;
+    width: 112px;
+    height: 56px;
+    align-items: flex-start;
+    padding: 10px;
+    background: #f9f8f8;
+    span {
+      font-size: 12px;
+      font-weight: 500;
+    }
+    p {
+      font-size: 14px;
+      font-weight: 500;
+    }
+  }
+`;
+
+const Profiles = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-top: 20px;
+  .profile {
+    display: flex;
+    text-decoration: none;
+    color: black;
+    align-items: center;
+    width: max-content;
+    cursor: pointer;
+    font-size: 12px;
+    padding: 5px 7px;
+    border-radius: 4px;
+    border: 1px solid #b0b0b0;
+    img {
+      width: 15px;
+    }
+  }
 `;
 
 const queryInOwnedToggleHandler = () => {
@@ -129,13 +190,66 @@ const queryInOwnedToggleHandler = () => {
 };
 
 const AboutOwner = styled.div`
-  height: 300px;
   background: ${isDarkModeOn ? "#1E2030" : "#FFF"};
+  .owner-details-main {
+    margin-left: 48px;
+  }
 `;
 const createStoreHandler = () => {
   // console.log("createStoreHandler");
   setOpen(true);
 };
+
+useEffect(() => {
+  asyncFetch(`https://api.mintbase.xyz/accounts/${accountId}`, {
+    method: "GET",
+    headers: {
+      "mb-api-key": "omni-site",
+      "Content-Type": "application/json",
+      "x-hasura-role": "anonymous",
+    },
+  }).then((data) => {
+    if (data.body) {
+      const parseData = data.body;
+      setProfile(parseData);
+    }
+  });
+}, []);
+
+console.log("profile", profile);
+
+const details = [
+  { name: "Tokens", value: "1075" },
+  { name: "Listed Tokens", value: "109" },
+  { name: "Bought", value: "161.18N" },
+  { name: "Sales", value: "189.41N" },
+  { name: "Transactions", value: "1776" },
+  { name: "Last Activity", value: "3 hours ago" },
+];
+
+const profiles = [
+  { icon: "twitter", link: "https://twitter.com/sharddog" },
+  { icon: "web", name: "Share.dog", link: "https://twitter.com/sharddog" },
+  {
+    icon: "simple_share",
+    name: "Simple Share",
+    link: "https://www.mintbase.xyz/contract/mint.sharddog.near/nfts/all/0?orderBy=price+desc+nulls+last#",
+  },
+  {
+    icon: "Claim Ownership",
+    name: "Claim Ownership",
+    link: "https://docs.google.com/forms/d/1w9QK9GXqmlRGLdS5Dm-yUoRdsd5klky89TwDEck35-M/viewform?edit_requested=true",
+  },
+  {
+    icon: "near",
+    name: "BOS",
+    link: "https://near.org/mintbase.near/widget/nft-marketplace?contracts=mint.sharddog.near",
+  },
+];
+
+// if (profile === null) {
+//   return "Loading";
+// }
 
 const PageContent = () => {
   switch (selectedTab) {
@@ -248,6 +362,11 @@ const modalContent = (
 
 const [count, setCount] = useState(0);
 
+const background = profile.backgroundImage
+  ? profile.headerImage ??
+    `https://ipfs.near.social/ipfs/${profile.backgroundImage.ipfs_cid}`
+  : "https://ipfs.near.social/ipfs/bafkreigtgmfmdoq66fuu6oepaddggslos3m7xyngja47zy2kuyicp3chay";
+
 return (
   <Card className={isDarkModeOn ? "dark" : ""}>
     <Widget
@@ -260,8 +379,79 @@ return (
         onRefresh: (sdk) => setSDK(sdk),
       }}
     />
-    <Toggle onClick={switchChangeHandler} title="Toggle Theme" />
-    <AboutOwner></AboutOwner>
+    <AboutOwner>
+      <ImageSection
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <img
+          src={
+            profile.image
+              ? profile.profileImage ??
+                `https://ipfs.near.social/ipfs/${profile.image.ipfs_cid}`
+              : "https://ipfs.near.social/ipfs/bafkreiajgp5bmkidwesy2d6tsbdkhyfzjtom2wse2sjcwii227lt5audvq"
+          }
+        />
+      </ImageSection>
+      <div className="owner-details-main">
+        <TopContent>
+          <h1>
+            {profile.displayName || profile.name} {verifiedBatch}
+          </h1>
+          <div className="contents">
+            <div className="content">
+              <p>Address</p>
+              <Widget
+                src={`/*__@appAccount__*//widget/Mintbase.MbActionText`}
+                props={{
+                  text: accountId,
+                  size: "medium",
+                  copyText: "mint.sharddog.near",
+                  link: "https://nearblocks.io/address/mint.sharddog.near",
+                  iconTab: false,
+                  iconCopy: false,
+                  mode: mode,
+                }}
+              />
+            </div>
+            {/* <div className="content">
+            <p>Created by</p>
+            <a>sharedog.near</a>
+          </div>
+          <div className="content">
+            <p>Category</p>
+            <a>Utility</a>
+          </div>
+          <div className="desc">The main ShardDog series based contract</div> */}
+          </div>
+        </TopContent>
+        <Details>
+          {details.map((data, key) => (
+            <div className="detail" key={key}>
+              <span>{data.name}</span>
+              <p>{data.value}</p>
+            </div>
+          ))}
+        </Details>
+        <Profiles>
+          {profiles.map((data, index) => (
+            <a href={data.link} target="_blank" key={index} className="profile">
+              <Widget
+                src="/*__@appAccount__*//widget/Mintbase.MbIcon"
+                props={{
+                  name: data.icon,
+                }}
+              />
+              <span>{data.name}</span>
+            </a>
+          ))}
+        </Profiles>
+      </div>
+    </AboutOwner>
     <Widget
       src={`/*__@appAccount__*//widget/Mintbase.MbTabs`}
       props={{
@@ -275,7 +465,9 @@ return (
     />
     <div
       className="d-flex flex-column align-items-center content_main"
-      style={{ backgroundColor: `${isDarkModeOn ? "#101223" : "#F9F9F9"}` }}
+      style={{
+        backgroundColor: `${isDarkModeOn ? "#101223" : "#F9F9F9"}`,
+      }}
     >
       <PageContent />
     </div>
