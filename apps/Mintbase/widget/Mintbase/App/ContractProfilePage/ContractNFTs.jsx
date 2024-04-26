6975e7cb033b4ac7b1d9ff3f477825baa3086267
Jiku Godwill <jikugodwill@yahoo.com>
2024-04-26 16:12:51 +0100

    Merge branch 'main' of https://github.com/GenaDrop/genadrop-bos-monorepo into staging

--- apps/Mintbase/widget/Mintbase/App/ContractProfilePage/ContractNFTs.jsx
+++ apps/Mintbase/widget/Mintbase/App/ContractProfilePage/ContractNFTs.jsx
@@ -57,58 +57,17 @@ const [loading, setLoading] = useState(true);
 const [countNFTs, setCountNFTs] = useState(0);
 const [pageNumber, setPageNumber] = useState(1);
 const [showListed, setShowListed] = useState(false);
+const perPage = 50;
 
-const limit = 20;
-
-const offset = (pageNumber - 1) * limit;
-const totalPages = Math.ceil(countNFTs / limit);
-
-// function fetchNFTs() {
-//   asyncFetch(
-//     `https://graph.mintbase.xyz/${
-//       contractId && contractId.endsWith(".testnet") ? "testnet" : "mainnet"
-//     }`,
-//     {
-//       method: "POST",
-//       headers: {
-//         "mb-api-key": "omni-site",
-//         "Content-Type": "application/json",
-//         "x-hasura-role": "anonymous",
-//       },
-//       body: JSON.stringify({
-//         query: `
-//       query MyQuery {
-//         mb_views_nft_tokens(
-//           where: {nft_contract_id: {_eq: "${contractId}"}}
-//           limit: ${limit}
-//           offset: ${offset}
-//         ) {
-//           nft_contract_id
-//           title
-//           media
-//           owner
-//         }
-//       }
-      
-//       `,
-//       }),
-//     }
-//   ).then((data) => {
-//     if (data.body.data?.mb_views_nft_tokens?.length) {
-//       setNftData(data.body.data?.mb_views_nft_tokens);
-//       setLoading(false);
-//       setCountNFTs(data.body.data?.mb_views_nft_tokens.length);
-//     }
-//   });
-// }
+const YoctoToNear = (offer_priceYocto) => {
+  return new Big(offer_priceYocto || 0)
+    .div(new Big(10).pow(24))
+    .toFixed(2)
+    .toString();
+};
 
 useEffect(() => {
-  // fetchNFTs({
-  //   contractId: contractId,
-  //   offset,
-  //   limit,
-  // });
-  getStoreNFTs({ offset, id: contractId, limit })
+  getStoreNFTs({ offset: (page - 1) * perPage, id: contractId, limit: perPage })
     .then(({ data, errors }) => {
       if (errors) {
         // handle those errors like a pro
@@ -124,7 +83,7 @@ useEffect(() => {
       // handle errors from fetch itself
       console.error(error);
     });
-}, [offset, pageNumber, showListed]);
+}, [limit, offset, pageNumber, showListed]);
 
 const listedToggleHandler = () => {
   setShowListed((prev) => !prev);
@@ -211,11 +170,18 @@ return (
               ))}
           </Cards>
           <div className="pagination_container">
-            <Pagination
-              totalPages={totalPages}
-              selectedPage={pageNumber}
-              onPageClick={(v) => setPageNumber(v)}
-            />
+            <p className="w-100 px-4">
+              <Widget
+                src="bos.genadrop.near/widget/Mintbase.TablePagination"
+                props={{
+                  totalItems,
+                  isDarkModeOn,
+                  itemsPerPage: countNFTs,
+                  currentPage: page,
+                  onPageChange: (page) => setPage(page),
+                }}
+              />
+            </p>
           </div>
         </>
       ) : (
