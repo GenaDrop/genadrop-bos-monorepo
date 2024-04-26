e06088075eaf3b3c8df43d2a65af67473a5b6f2c
Jiku Godwill <jikugodwill@yahoo.com>
2024-04-26 12:41:17 +0100

    Fixed StoreFront UI and links

--- apps/Mintbase/widget/Mintbase/App/Index.jsx
+++ apps/Mintbase/widget/Mintbase/App/Index.jsx
@@ -136,11 +136,11 @@ const config = {
         path: "${config_account}/widget/Mintbase.App.ContractProfilePage.Index",
         blockHeight: "final",
         init: {
-          name: "Human",
+          name: "StoreFront",
           right: {
             one: [
-              { name: "Owned", tab: "owned" },
-              { name: "minted", tab: "minted" },
+              { name: "NFTs", tab: "nfts" },
+              { name: "mint NFT", tab: "mint-nft" },
               { name: "About", tab: "about" },
             ],
             two: [
