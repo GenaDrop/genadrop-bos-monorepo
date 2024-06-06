07232f09fe09b2a5c83816424e8cf1745fda84fa
Jiku Godwill <jikugodwill@yahoo.com>
2024-06-06 09:38:46 +0100

    Fixed app width on other gateways

--- apps/Mintbase/widget/Mintbase/App/Layout.jsx
+++ apps/Mintbase/widget/Mintbase/App/Layout.jsx
@@ -1,33 +1,48 @@
+const data = fetch(`https://httpbin.org/headers`);
+const gatewayURL = data?.body?.headers?.Origin ?? "";
+
+const Container = gatewayURL.includes("near.org")
+  ? styled.div`
+      width: 100%;
+    `
+  : styled.div`
+      position: fixed;
+      inset: 73px 0px 0px;
+      width: 100%;
+      overflow-y: scroll;
+    `;
 return {
   Layout: ({ variant, blocks, children, isHome }) => {
     switch (variant) {
       case "standard": {
         const { Header, Footer } = blocks;
         return (
-          <div
-            className="layout"
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              width: "100%",
-              height: "100%",
-            }}
-          >
-            <Header isHome={isHome.page === "home"} />
+          <Container>
             <div
-              className="content"
+              className="layout"
               style={{
-                flex: 1,
                 display: "flex",
                 flexDirection: "column",
-                alignItems: "center",
                 width: "100%",
+                height: "100%",
               }}
             >
-              {children}
+              <Header isHome={isHome.page === "home"} />
+              <div
+                className="content"
+                style={{
+                  flex: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  width: "100%",
+                }}
+              >
+                {children}
+              </div>
+              <Footer />
             </div>
-            <Footer />
-          </div>
+          </Container>
         );
       }
     }
